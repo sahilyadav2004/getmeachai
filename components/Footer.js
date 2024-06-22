454a9efe1dc1d5d51cdb0d5365302ec9fd98be10
sahilyadav2004 <sahillawaniya4@gmail.com>
2024-06-22 12:16:37 +0530

    done

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-blue-950 text-white flex justify-center px-4 items-center h-16'>
+      <p className='text-center'>
+        Copyright &copy; Get me a Chai -Fund your project with chai
+    </p>
+    </footer>
+  )
+}
+
+export default Footer
